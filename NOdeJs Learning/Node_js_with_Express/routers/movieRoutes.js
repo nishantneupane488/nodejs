afa984889a7494edad1183a0f6a4c8a988a1c5e3
nishantneupane488 <np03cs4s220425@heraldcollege.edu.np>
2023-05-06 03:10:55 +0545

    feats: making files and code in systematic order

---
+++ NOdeJs Learning/Node_js_with_Express/routers/movieRoutes.js
@@ -0,0 +1,33 @@
+const express =require('express')
+const moviesCOntroller = require('./../controllers/moviesController')
+
+const router = express.Router();
+router.route('/')
+ .get(moviesCOntroller.getAllMovie)
+ .post(moviesCOntroller.createMovie)
+
+ router.route('/:id')
+.get(moviesCOntroller.getMovie)
+.patch(moviesCOntroller.UpdateMovie)
+.delete(moviesCOntroller.deleteMovie)
+module.exports = router;
+
+
+
+
+     // //                GET - API/V1/MOVIES/ID
+     
+    //  app.get('/api/v1/movies',getAllMovie)
+    //  app.get('/api/v1/movies/:id',getMovie)
+    //  //                POST - API/V1/MOVIES                  //BOTH LOWERCODE AND THIS CODE IS SAME
+    //  app.post('/api/v1/movies',createMovie);
+    //  // USING PATCH METHOD TO UPDATE DATA
+    //  app.patch('/api/v1/movies/:id',UpdateMovie);
+    //  app.delete('/api/v1/movies/:id',deleteMovie);
+    //  app.route('/api/v1/movies')
+    //  .get(getAllMovie)
+    //  .post(createMovie)
+    // app.route('/api/v1/movies/:id')
+    // .get(getMovie)
+    // .patch(UpdateMovie)
+    // .delete(deleteMovie)
